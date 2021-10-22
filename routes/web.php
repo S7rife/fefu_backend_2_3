ceb6d7bcb44b04195aa2996bcdf1ff9a172996d7
S7rife <81598492+S7rife@users.noreply.github.com>
2021-10-23 00:14:09 +1000

    Lesson 4: Add news routes, controller and views (#3)
    * Lesson 1: Switch MySQL to PostgreSQL, Add pgAdmin Docker container config
    
    * Lesson 3: Add news model, migration, factory and seeder
    
    * Lesson 4: Add news routes, controller and views
    
    * Lesson 4: Delete comments and duplicated code, add news_list route, moved datetime
    
    Co-authored-by: chrn-feip <40585429+chrn-feip@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\NewsController;
+use App\Models\News;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/news', [NewsController::class, 'getList'])->name('news_list');
+Route::get('/news/{slug}', [NewsController::class, 'getDetails'])->name('news_item');
