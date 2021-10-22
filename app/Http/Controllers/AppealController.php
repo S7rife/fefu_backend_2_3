b1b0a51bb6ec796361dc9a82ee4104d918d78fe2
S7rife <81598492+S7rife@users.noreply.github.com>
2021-10-23 00:38:51 +1000

    Lesson 5: Add appeal form (#4)
    * Lesson 1: Switch MySQL to PostgreSQL, Add pgAdmin Docker container config
    
    * Lesson 3: Add news model, migration, factory and seeder
    
    * Lesson 4: Add news routes, controller and views
    
    * Lesson 5: Add appeal form
    
    * Lesson 5: Removed checks, type change
    
    Co-authored-by: chrn-feip <40585429+chrn-feip@users.noreply.github.com>

---
+++ app/Http/Controllers/AppealController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Appeal;
+use Illuminate\Http\Request;
+
+class AppealController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+
+        $errors = [];
+        $success = $request->session()->get('success', false);
+
+        if ($request->isMethod('post')) {
+            $name = $request->input('name');
+            $message = $request->input('message');
+            $phone = $request->input('phone');
+            $email = $request->input('email');
+
+            if ($name === null) {
+                $errors['name'] = 'Is your name so empty?';
+            }
+
+            if ($message === null) {
+                $errors['message'] = 'Well, write at least something!';
+            }
+
+            if ($phone === null && $email === null) {
+                $errors['contacts'] = 'We just need to contact you somehow';
+            }
+
+
+            if (count($errors) > 0) {
+                $request->flash();
+            } else {
+                $appeal = new Appeal();
+                $appeal->name = $name;
+                $appeal->message = $message;
+                $appeal->phone = $phone;
+                $appeal->email = $email;
+                $appeal->save();
+
+                $success = true;
+
+                return redirect()
+                    ->route('appeal')
+                    ->with('success', $success);
+            }
+        }
+        return view('appeal', ['errors' => $errors, 'success' => $success]);
+    }
+}
