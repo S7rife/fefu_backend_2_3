bd00891bc7dc097b1fc1304cac69924853785e87
_//Dmitriy_Shashkov//‾ <81598492+S7rife@users.noreply.github.com>
2021-11-05 19:59:57 +1000

    Lesson 7: Add redirecting middleware (#6)
    * Lesson 7: Add redirecting middleware
    
    * Lesson 7: Add attributes and new check, fix redirect middleware and ChangeNewsSlug command
    
    * Lesson 7: Fix redirect, remove hardcode
    
    * Lesson 7: remove echo from line 28 of the RedirectFromOldSlug.php

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,11 +2,21 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
+/**
+ * @property string $title
+ * @property string $slug
+ * @property boolean $is_published
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Carbon $published_at
+ * @property string|null $description
+ * @property string $text
+ */
 class News extends Model
 {
     use HasFactory, Sluggable;
@@ -18,5 +28,20 @@ class News extends Model
                 'source' => 'title'
             ]
         ];
+    }
+
+    public function save(array $options = [])
+    {
+        if ($this->exists && $this->isDirty('slug'))
+        {
+            $oldSlug = $this->getOriginal('slug');
+            $newSlug = $this->slug;
+
+            $redirect = new Redirect();
+            $redirect->old_slug = route('news_item', ['slug' => $oldSlug], false);
+            $redirect->new_slug = route('news_item', ['slug' => $newSlug], false);
+            $redirect->save();
+        }
+        return parent::save($options);
     }
 }
