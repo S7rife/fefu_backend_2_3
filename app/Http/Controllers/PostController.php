29351ca7b73986b07f0e75d76d8cbdac4de4a7db
s7rife <dimonchik22824@gmail.com>
2021-11-20 21:58:34 +1000

    Lesson 9: Add posts and comments with API

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use function response;
+
+class PostController extends Controller
+{
+    private const PAGE_SIZE = 5;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(): JsonResponse
+    {
+        $posts = Post::with('user')->ordered()->paginate(self::PAGE_SIZE);
+        return response()->json(PostResource::collection($posts));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|between: 5, 30',
+            'text' => 'required|between: 250, 500',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()->all()], 422);
+        }
+
+
+        $validated = $validator->validated();
+
+        $post = new Post();
+        $post->title = $validated['title'];
+        $post->text = $validated['text'];
+        $post->user_id = User::inRandomOrder()->first()->id;
+        $post->save();
+
+        return response()->json(new PostResource($post), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post): JsonResponse
+    {
+        return response()->json(new PostResource($post));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Post $post
+     * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Request $request, Post $post): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'sometimes|required|between: 5, 30',
+            'text' => 'sometimes|required|between: 250, 500',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()->all()], 422);
+        }
+
+
+        $validated = $validator->validated();
+
+        if (isset($validated['title']))
+            $post->title = $validated['title'];
+
+        if (isset($validated['text']))
+            $post->text = $validated['text'];
+
+        $post->save();
+
+        return response()->json(new PostResource($post));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post): JsonResponse
+    {
+        $post->delete();
+        return response()->json(['message' => 'Post removed successfully']);
+    }
+}
